9495a53773bb4834ae553f062d652abdf7038d77
Maxwell Farrell <richard.farrell2@sky.uk>
2017-03-27 23:38:58 +0100

    Some stuff

--- src/transactions/inline-category-form.js
+++ src/transactions/inline-category-form.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Control } from 'react-redux-form'
 import './inline-category-form.scss'
+
+class CategorySelect extends Component {
+  render () {
+    return (
+      <Control.select model='user.faveColor' className='form-control form-control-sm'>
+        <option value='red'>red</option>
+        <option value='green'>green</option>
+        <option value='blue'>blue</option>
+      </Control.select>
+    )
+  }
+}
 
 class CategoryForm extends Component {
   render () {
     return (
       <div className='categoryForm form-group form-inline'>
         Uncategorized
-        <Control.select model='user.faveColor' className='form-control form-control-sm'>
-          <option value='red'>red</option>
-          <option value='green'>green</option>
-          <option value='blue'>blue</option>
-        </Control.select>
+        {
+          this.props.categories.length > 0 ? <CategorySelect /> : <Control.text />
+        }
         <button type='button' className='btn btn-sm btn-outline-primary'>New Category</button>
       </div>
     )
   }
 }
 
-export default CategoryForm
+const mapStateToProps = state => {
+  return { categories: state.categories.entries }
+}
+
+export default connect(mapStateToProps)(CategoryForm)
