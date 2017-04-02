69b10b663ba2d058515ffdf4b9193ee5be486fba
Maxwell Farrell <richard.farrell2@sky.uk>
2017-04-02 19:24:54 +0100

    Dashboard category overview

--- src/dashboard/home/category-overview.js
+++ src/dashboard/home/category-overview.js
@@ -3,14 +3,20 @@ import './category-overview.scss'
 
 class CategoryOverview extends Component {
   render () {
+    console.log(this.props)
+    const { category } = this.props
+
     return (
       <div className='categoryOverview card'>
         <div className='card-header'>
-          <h5 className='title'>{this.props.category}</h5>
+          <h5 className='title'>{category}</h5>
         </div>
         <div className='card-block'>
           <p className='info'>
             <span className='badge badge-default'>£50</span>Spent this month
+          </p>
+          <p className='info'>
+            <span className='badge badge-default'>{this.props.transactions.length}</span>Transactions
           </p>
         </div>
         <div className='card-footer text-muted'>
--- src/dashboard/home/home.js
+++ src/dashboard/home/home.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import R from 'ramda'
 import CategoryOverview from './category-overview'
 import './home.scss'
 
 class Home extends Component {
   render () {
+    const { categories, transactions } = this.props
+    const categoryIds = Object.keys(categories)
+    const uncategorized = R.values(transactions).filter(t => !t.metadata.category)
+
     return (
       <div className='dashboard-home'>
         <div className='container-fluid'>
           <div className='row'>
+            <div className='col-xl-3'>
+              <CategoryOverview
+                category='Uncategorized'
+                transactions={uncategorized} />
+            </div>
             {
-              this.props.categories.map(c => {
+              categoryIds.map(c => {
+                const transactionsForCategory = this.props.transactions.filter(t => t.metadata.category === c)
+                console.log(transactionsForCategory)
+
                 return (
                   <div className='col-xl-3'>
-                    <CategoryOverview category={c} key={c} />
+                    <CategoryOverview
+                      category={categories[c]}
+                      transactions={transactionsForCategory}
+                      key={c} />
                   </div>
                 )
               })
@@ -26,8 +42,10 @@ class Home extends Component {
 }
 
 const mapStateToProps = state => {
-  const categoriesById = Object.keys(state.categories.byId)
-  return { categories: categoriesById.map(id => state.categories.byId[id]) }
+  return {
+    categories: state.categories.byId,
+    transactions: R.values(state.transactions.byId)
+  }
 }
 
 export default connect(mapStateToProps)(Home)
