36b82b8593a2651c09afa0ee792134bc50931a12
Maxwell Farrell <richard.farrell2@sky.uk>
2017-04-09 23:40:07 +0100

    re order

--- src/top-nav/side-bar/menu.js
+++ src/top-nav/side-bar/menu.js
@@ -6,17 +6,24 @@ export default class Menu extends Component {
   render () {
     return (
       <div className='side-bar'>
-        <SideMenu noOverlay width={'250'} isOpen={this.props.open} customBurgerIcon={false} customCrossIcon={false}>
+        <SideMenu noOverlay width={'235'} isOpen={this.props.open} customBurgerIcon={false} customCrossIcon={false}>
           <p className='sidebar__divider'>Dashboard</p>
           <p className='sidebar__divider'>Categories</p>
           <p>Transactions</p>
           <p className='last-updated text-muted'>
             <small>Updated 50 mins ago</small>
           </p>
-          <p className='sidebar__divider'>New Transactions</p>
-          <p>
+          <p className='sidebar__divider'>
             Uncategorized
             <span className='badge badge-warning'>10</span>
+          </p>
+          <p className='sidebar__divider'>
+            New Transactions
+            <span className='badge badge-warning'>9</span>
+          </p>
+          <p>
+            Newly Exported
+            <span className='badge badge-warning'>8</span>
           </p>
         </SideMenu>
       </div>
