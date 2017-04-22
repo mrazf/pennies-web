50d85f92ef85ef49922d218ed3f17c4800f31c36
Maxwell Farrell <richard.farrell2@sky.uk>
2017-04-22 18:25:16 +0100

    side bar wrangle

--- src/top-nav/side-bar/menu.js
+++ src/top-nav/side-bar/menu.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router'
 import { slide as SideMenu } from 'react-burger-menu'
 import './menu.scss'
 
+const styles = {
+  bmMenuWrap: { position: 'absolute' }
+}
+
 export default class Menu extends Component {
   render () {
     return (
       <div className='side-bar'>
-        <SideMenu noOverlay width={'235'} isOpen={this.props.open} customBurgerIcon={false} customCrossIcon={false}>
+        <SideMenu noOverlay styles={styles} width={235} isOpen={this.props.open} customBurgerIcon={false} customCrossIcon={false}>
           <p className='sidebar__divider'>Dashboard</p>
           <p className='sidebar__divider'>Categories</p>
           <p>Transactions</p>
