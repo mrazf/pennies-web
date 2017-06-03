2a2ebf99b155ae48a65e1b3050a3a641e31c5641
Maxwell Farrell <richard.farrell2@sky.uk>
2017-06-03 12:17:43 +0100

    menu

--- src/top-nav/side-bar/menu.js
+++ src/top-nav/side-bar/menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
+import { Link, withRouter } from 'react-router'
 import { slide as SideMenu } from 'react-burger-menu'
 import './menu.scss'
 
@@ -7,25 +7,35 @@ const styles = {
   bmMenuWrap: { position: 'absolute' }
 }
 
-export default class Menu extends Component {
+class Menu extends Component {
   render () {
     return (
       <div className='side-bar'>
         <SideMenu noOverlay styles={styles} width={235} isOpen={this.props.open} customBurgerIcon={false} customCrossIcon={false}>
           <p className='sidebar__divider'>
-            <Link to='/dashboard'>Dashboard</Link>
+            <Link to='/dashboard' activeClassName='active'>
+              Dashboard
+            </Link>
           </p>
           <p className='sidebar__divider'>
-            <Link to='/dashboard/categories'>Categories</Link>
+            <Link to='/dashboard/categories' activeClassName='active'>
+              Categories
+            </Link>
           </p>
           <p className='sidebar__divider'>
-            <Link to='/dashboard/transactions'>Transactions</Link>
+            <Link to='/dashboard/transactions' activeClassName='active'>
+              Transactions
+            </Link>
           </p>
           <p>
-            <Link to='/dashboard/exporter'>Exporter</Link>
+            <Link to='/dashboard/exporter' activeClassName='active'>
+              Exporter
+            </Link>
           </p>
         </SideMenu>
       </div>
     )
   }
 }
+
+export default withRouter(Menu)
