3ffdecb3d1de830a759826a73ff2f33a3dc3410a
Maxwell Farrell <richard.farrell2@sky.uk>
2017-06-06 22:05:59 +0100

    seperate transactions table

---
+++ src/transactions/table.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+import * as R from 'ramda'
+import TransactionContainer from './transaction/transaction-container'
+
+class Table extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { expanded: false }
+
+    this.expand = () => this.setState({ expanded: true })
+    this.contract = () => this.setState({ expanded: false })
+  }
+
+  render () {
+    const ids = Object.keys(this.props.transactions)
+    const transactions = ids.map(id => this.props.transactions[id]).reverse()
+    const tableClasses = classnames('table table-sm', { expanded: this.state.expanded })
+
+    return (
+      <table className={tableClasses}>
+        <thead>
+          <tr>
+            <th>Date & Time</th>
+            <th>Category</th>
+            <th className='amount-header'>Amount</th>
+            <th>Merchant</th>
+            <th>Monzo Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            transactions.map((t, i) => {
+              const updating = R.contains(t.id, this.props.updatingTransactions)
+
+              return (
+                <TransactionContainer
+                  rowIndex={i}
+                  updating={updating}
+                  transaction={t}
+                  expand={this.expand}
+                  contract={this.contract}
+                  key={t.id}
+                />
+              )
+            })
+          }
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export default Table
--- src/transactions/transactions.js
+++ src/transactions/transactions.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import * as R from 'ramda'
-import classnames from 'classnames'
-import TransactionContainer from './transaction/transaction-container'
 import TimeRangeContainer from './time-range-container'
 import HeadlinesContainer from './headlines/headlines-container'
-import { updateCategory } from './redux-actions'
+import Table from './table'
 import './transactions.scss'
 
 class Transactions extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = { expanded: false }
-
-    this.expand = () => this.setState({ expanded: true })
-    this.contract = () => this.setState({ expanded: false })
-  }
-
   render () {
-    const ids = Object.keys(this.props.transactions)
-    const transactions = ids.map(id => this.props.transactions[id]).reverse()
-    const tableClasses = classnames('transactions__table', { expanded: this.state.expanded })
-
     return (
       <div className='transactions container'>
         <div className='transactions__picker row'>
@@ -33,39 +17,12 @@ class Transactions extends Component {
             <HeadlinesContainer selectedMonth={this.props.selectedMonth} />
           </div>
         </div>
-        <div className='row'>
+        <div className='transactions__table row'>
           <div className='col'>
-            <div className={tableClasses}>
-              <table className='table table-sm'>
-                <thead>
-                  <tr>
-                    <th>Date & Time</th>
-                    <th>Category</th>
-                    <th className='amount-header'>Amount</th>
-                    <th>Merchant</th>
-                    <th>Monzo Category</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    transactions.map((t, i) => {
-                      const updating = R.contains(t.id, this.props.updatingTransactions)
-
-                      return (
-                        <TransactionContainer
-                          rowIndex={i}
-                          updating={updating}
-                          transaction={t}
-                          expand={this.expand}
-                          contract={this.contract}
-                          key={t.id}
-                        />
-                      )
-                    })
-                  }
-                </tbody>
-              </table>
-            </div>
+            <Table
+              transactions={this.props.transactions}
+              updatingTransactions={this.props.updatingTransactions}
+            />
           </div>
         </div>
       </div>
@@ -77,15 +34,10 @@ const mapStateToProps = state => {
   return {
     transactions: state.transactions.byId,
     updatingTransactions: state.transactions.updatingIds,
-    selectedMonth: state.transactions.selectedMonth,
-    categories: state.categories.byId
+    selectedMonth: state.transactions.selectedMonth
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return { updateCategory: (transaction, categoryUpdate) => dispatch(updateCategory(transaction, categoryUpdate)) }
-}
-
-const TransactionsContainer = connect(mapStateToProps, mapDispatchToProps)(Transactions)
+const TransactionsContainer = connect(mapStateToProps)(Transactions)
 
 export default TransactionsContainer
